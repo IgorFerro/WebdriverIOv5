bbe23487aabb866e64ede101f581a45542593ddf
Igor Ferro Parreira <ifparreira@uolinc.com>
2019-12-27 07:02:01 -0300

    close tab

---
+++ test/close-tab-test.js
@@ -0,0 +1,20 @@
+describe("Selectors Test", () => {
+    beforeEach(function() {
+      browser.setWindowSize(1600, 1000);
+      browser.url("./");
+    });
+  
+    it("By ID", () => {
+      const clickById = $("#contact-us");
+      clickById.click();
+
+      console.log('HOMEPAGE HEADER:' + browser.getTitle());
+      browser.pause(2000);
+
+      browser.switchWindow('WebDriver | Contact Us');
+      console.log('CONTACT US HEADER:' + browser.getTitle());
+      browser.closeWindow();
+      browser.pause(2000);
+
+    });
+});
